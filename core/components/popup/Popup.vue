d2fc9b5237e65884996c0707859ea19f706810ff
hongsh <hongsh@dpark.com.cn>
2022-08-29 19:55:36 +0800

    移除修改

--- core/components/popup/Popup.vue
+++ core/components/popup/Popup.vue
@@ -3,7 +3,6 @@
     v-show="popupVisible"
     ref="popup"
     class="my-popup esri-component esri-popup esri-popup--aligned-top-center esri-popup--shadow"
-    :class="{ dockered: dockered }"
   >
     <div
       class="esri-popup__main-container esri-widget esri-popup--is-collapsible"
